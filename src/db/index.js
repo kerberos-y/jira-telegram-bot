7d810d560ac96da70921290e495d0ba9118df678
mitiatalanoff <mitiatalanoff@gmail.com>
2026-04-22 17:24:07 +0300

    фвв вщслук ещ инзфыы зкщидуь цшер утм

--- src/db/index.js
+++ src/db/index.js
@@ -1,28 +1,16 @@
 const { Pool } = require('pg');
 
-let _pool = null;
-
-function getPool() {
-  if (_pool) return _pool;
-
-  if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL is not set!');
-  }
-
-  _pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }, // Railway требует SSL
-  });
-
-  _pool.on('error', (err) => {
-    console.error('PostgreSQL pool error:', err);
-  });
-
-  return _pool;
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set!');
 }
 
-module.exports = new Proxy({}, {
-  get(_target, prop) {
-    return getPool()[prop];
-  },
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // Railway требует SSL
 });
+
+pool.on('error', (err) => {
+  console.error('PostgreSQL pool error:', err);
+});
+
+module.exports = pool;
