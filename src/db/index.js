ab39c875e25913a7eb769250529cdb71d8e9fca7
mitiatalanoff <mitiatalanoff@gmail.com>
2026-04-22 16:52:04 +0300

    ввв

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,16 @@
 const { Pool } = require('pg');
 
+console.log('[DEBUG] DATABASE_URL:', process.env.DATABASE_URL || 'UNDEFINED');
+console.log('[DEBUG] NODE_ENV:', process.env.NODE_ENV || 'UNDEFINED');
+console.log('[DEBUG] All env keys:', Object.keys(process.env).join(', '));
+
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not set!');
 }
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, // Railway требует SSL
+  ssl: { rejectUnauthorized: false },
 });
 
 pool.on('error', (err) => {
