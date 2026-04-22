9f4c78cb73d4d2ca4030d01671877ea45aa83482
mitiatalanoff <mitiatalanoff@gmail.com>
2026-04-22 16:58:26 +0300

    fix problev vith env in railway

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,12 @@
 const { Pool } = require('pg');
 
-console.log('[DEBUG] DATABASE_URL:', process.env.DATABASE_URL || 'UNDEFINED');
-console.log('[DEBUG] NODE_ENV:', process.env.NODE_ENV || 'UNDEFINED');
-console.log('[DEBUG] All env keys:', Object.keys(process.env).join(', '));
-
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not set!');
 }
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false }, // Railway требует SSL
 });
 
 pool.on('error', (err) => {
